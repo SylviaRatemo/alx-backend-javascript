f20d811662560a4f638148a27707e3fa2b3d7c63
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-09-27 14:48:01 +0300

    .all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ const { uploadPhoto, createUser } = require('./utils');
 export default function handleProfileSignup(){
   return Promise.all([uploadPhoto(), createUser()])
   .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
   })
   .catch(() => console.log("Signup system offline"));
 }
