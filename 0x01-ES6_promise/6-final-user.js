d09befbe7ab527958bbef3250868332f714cb1f6
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-09-27 16:51:51 +0300

    Lint errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-  .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then((res) => (
-    res.map((r) => ({
-      status: r.status,
-      value: r.status === 'fulfilled' ? r.value : String(r.reason),
-    }))
-  ));
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((r) => ({
+        status: r.status,
+        value: r.status === 'fulfilled' ? r.value : String(r.reason),
+      }))
+    ));
 }
