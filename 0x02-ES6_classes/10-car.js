14a31e2c27d9bf6d55ce3eb242934508de65ed61
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-09-28 18:55:20 +0300

    Symbols ES6

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,40 +1,40 @@
 export default class Car {
-    constructor (brand, motor, color) {
-        this.brand = brand;
-        this.motor = motor;
-        this.color = color;
-    }
-    
-    set brand(value) {
-        this._brand = value;
-    }
+ constructor (brand, motor, color) {
+      this.brand = brand;
+      this.motor = motor;
+      this.color = color;
+  }
+ 
+  set brand(value) {
+      this._brand = value;
+  }
 
-    get brand() {
-        return this._brand;
-    }
+  get brand() {
+      return this._brand;
+  }
 
-    set motor(value) {
-        this._motor = value;
-    }
+  set motor(value) {
+      this._motor = value;
+  }
 
-    get motor() {
-        return this._motor;
-    }
+  get motor() {
+      return this._motor;
+  }
 
-    set color(value) {
-        this._color = value;
-    }
+  set color(value) {
+      this._color = value;
+  }
 
-    get color() {
-        return this._color;
-    }
+  get color() {
+      return this._color;
+  }
 
-    static get [Symbol.species]() {
-        return this;
-    }
+  static get [Symbol.species]() {
+      return this;
+  }
 
-    cloneCar() {
-        const clone = this.constructor[Symbol.species];
-        return new clone();
-    }
+  cloneCar() {
+      const clone = this.constructor[Symbol.species];
+      return new clone();
+  }
 }
