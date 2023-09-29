e7e0b4d31316741f4dc3e41f0dd99186480fcb3f
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-09-29 11:45:41 +0300

    for of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const arr = array;
   for (const idx of array) {
     const value = array[idx];
-    arr[idx] += appendString + value;
+    arr[idx] = appendString + value;
   }
 
   return arr;
