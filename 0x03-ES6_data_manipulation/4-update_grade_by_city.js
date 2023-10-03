05ffbd93d11a245cce51578d94550b255f34e888
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-03 12:03:08 +0300

    map and filter combined

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-param-reassign */
 export default function updateStudentGradeByCity(arr, city, gradeArr) {
-  if (gradeArr.grade === undefined) {
-    gradeArr.grade = 'N/A';
-  }
   const students = arr.filter((newArr) => newArr.location === city);
   const final = students.map((x) => {
     for (const grade of gradeArr) {
       if (x.id === grade.studentId) {
         x.grade = grade.grade;
       }
+    }
+    if (gradeArr.grade === undefined) {
+      gradeArr.grade = 'N/A';
     }
     return x;
   });
