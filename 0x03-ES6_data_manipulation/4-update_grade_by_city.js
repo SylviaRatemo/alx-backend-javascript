b3aa55fc425b90a3fe81dab12c3ef34d8abc7cb5
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-03 12:32:28 +0300

    map and filter

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-param-reassign */
-export default function updateStudentGradeByCity(arr, city, gradeArr) {
+export default function updateStudentGradeByCity(arr, city, newGrades) {
   const students = arr.filter((newArr) => newArr.location === city);
   const final = students.map((student) => {
-    for (const grade of gradeArr) {
+    for (const grade of newGrades) {
       // eslint-disable-next-line no-prototype-builtins
       if (!grade.hasOwnProperty('grade') || (grade.grade === undefined)) {
         grade.grade = 'N/A';
