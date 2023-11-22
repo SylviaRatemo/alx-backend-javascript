92e8ecc1fd94e1f6a8b9cfefab8982d15be3211a
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-22 09:20:24 +0300

    Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,31 +5,29 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf-8');
     const lines = data.split('\n');
     const nonEmptyLines = lines.filter((line) => line.trim() !== '');
-    const count = nonEmptyLines.length;
 
-    console.log(`Number of students: ${count}`);
+    console.log(`Number of students: ${nonEmptyLines.length}`);
 
     const header = nonEmptyLines[0].split(',');
-
     const columnName = 'field';
     const firstnameColumn = 'firstname';
 
     const columnIndex = header.indexOf(columnName);
     const firstnameIndex = header.indexOf(firstnameColumn);
 
-    const fieldFirstNameMap = {};
-
-    nonEmptyLines.slice(1).forEach((line) => {
+    const fieldFirstNameMap = nonEmptyLines.slice(1).reduce((map, line) => {
       const values = line.split(',');
       const field = values[columnIndex].trim();
       const firstname = values[firstnameIndex].trim();
 
-      if (!fieldFirstNameMap[field]) {
-        fieldFirstNameMap[field] = [];
+      if (!map[field]) {
+        // eslint-disable-next-line no-param-reassign
+        map[field] = [];
       }
 
-      fieldFirstNameMap[field].push(firstname);
-    });
+      map[field].push(firstname);
+      return map;
+    }, {});
 
     // Log the results
     Object.entries(fieldFirstNameMap).forEach(([field, firstnames]) => {
