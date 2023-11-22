dc0697f8efad1eeb61c770eebea99d980d3ee631
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-22 20:20:52 +0300

    Task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,8 +1,23 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const [, , databaseFileName] = process.argv;
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is the list of our students');
+    countStudents(databaseFileName)
+      .then(() => {
+        console.log('Done!');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 });
 
 app.listen(1245, () => {
