7a80fa39f27ab0ef6b6c17d90cf1c3334a2f26b7
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-22 07:49:47 +0300

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // Listen for user input on stdin
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
 
   // Close the program
   process.nextTick(() => {
