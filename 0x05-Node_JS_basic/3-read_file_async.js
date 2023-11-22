89c8d72e35cc648b03daeea2666126dad3402508
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-22 12:30:04 +0300

    Task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  // try-catch to handle errors
+  try {
+    const fileLines = fs.readFileSync(dataPath, 'utf-8').trim().split('\n');
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+
+      // eslint-disable-next-line no-prototype-builtins
+      if (!studentGroups.hasOwnProperty(field)) {
+        studentGroups[field] = [];
+      }
+
+      // eslint-disable-next-line max-len
+      const studentEntries = studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object
+      .values(studentGroups)
+      .reduce((pre, cur) => pre + cur.length, 0);
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const [field, group] of Object.entries(studentGroups)) {
+      const studentNames = group.map((student) => student.firstname).join(', ');
+      console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    }
+    const result = {
+      totalStudents,
+      studentGroups,
+    };
+    resolve(result);
+  } catch (error) {
+    reject(new Error('Cannot load the database'));
+  }
+});
+
+module.exports = countStudents;
