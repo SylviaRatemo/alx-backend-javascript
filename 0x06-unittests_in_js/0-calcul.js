cd2ac33383389f09c761ef4aee6bc71b717c3deb
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-23 19:58:13 +0300

    ENgineer

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,5 @@
 function calculateNumber(a, b) {
-  const an = Math.round(a);
-  const bn = Math.round(b);
-  return an + bn;
+  return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
