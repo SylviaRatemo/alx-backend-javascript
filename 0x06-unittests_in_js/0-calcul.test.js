27ccc2df607a10cb573933a9144307eabd0850bb
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-23 20:23:09 +0300

    Engineer

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,19 +3,19 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('floating point whole numbers', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
   it('rounding down b\'s floating point fractional number', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
   it('rounding down a and b\'s floating point fractional number', () => {
-    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
   it('rounding down a\'s floating point fractional number', () => {
-    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 
   it('rounding up b\'s floating point fractional numbers', () => {
