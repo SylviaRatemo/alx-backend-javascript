69a8059a00f69ac8bac646b00d2fa97006be9a34
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-24 16:28:53 +0300

    Man

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,12 +1,17 @@
-// 0-calcul.test.js
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-    it('should return the sum of rounded numbers', function () {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    });
+describe('calculateNumber', () => {
+  it('whole numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('second number floating point', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('both numbers floating point', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
