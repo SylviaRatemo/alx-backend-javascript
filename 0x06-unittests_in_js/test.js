cf2195d5be62aee0bef1ed9ba66d7e4c4bc05dd8
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-24 08:35:52 +0300

    Task 0

--- 0x06-unittests_in_js/test.js
+++ 0x06-unittests_in_js/test.js
@@ -1,20 +1,20 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const assert = require("assert");
+const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', () => {
-  it('round the first argument', () => {
+  it('it round the first argument', () => {
     assert.equal(calculateNumber(1.0, 0), 1);
     assert.equal(calculateNumber(1.3, 0), 1);
     assert.equal(calculateNumber(1.7, 0), 2);
   });
 
-  it('round the second argument', () => {
+  it('it round the second argument', () => {
     assert.equal(calculateNumber(0, 1.0), 1);
     assert.equal(calculateNumber(0, 1.3), 1);
     assert.equal(calculateNumber(0, 1.7), 2);
   });
 
-  it('should return the right number', () => {
+  it('it should return the right number', () => {
     assert.equal(calculateNumber(1.3, 0), 1);
     assert.equal(calculateNumber(0, 1.2), 1);
     assert.equal(calculateNumber(1.3, 1.3), 2);
